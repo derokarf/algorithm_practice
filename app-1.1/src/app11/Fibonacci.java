bcad2cca64c0b829acf2aed55830fd3aee7b9f23
derokarf <derokarf@gmail.com>
2017-04-19 22:36:37 +0800

    Для любого N

--- app-1.1/src/app11/Fibonacci.java
+++ app-1.1/src/app11/Fibonacci.java
@@ -12,28 +12,39 @@ import libBook.StdOut;
  *
  * @author igor
  */
-public class Fibonacci 
-{
-	static BigInteger[] listFib = new BigInteger[100];
-	
-    public static BigInteger F(int N) 
-    {
+public class Fibonacci {
+
+	static BigInteger[] listFib;
+
+	public static BigInteger F(int N) {
 		
-        if (N == 0){
+		if(N>=0){
+			listFib = new BigInteger[N + 1];
+			return calcF(N);			
+		}else{
+			StdOut.println("N должно быть целым неотрицательным числом");
+			return BigInteger.valueOf(0);
+		}
+	}
+
+	private static BigInteger calcF(int N) {
+		if (N == 0) {
 			listFib[N] = BigInteger.valueOf(N);
 			return BigInteger.valueOf(0);
 		}
-        if (N == 1){
+		if (N == 1) {
 			listFib[N] = BigInteger.valueOf(N);
-			return BigInteger.valueOf(0);
+			return BigInteger.valueOf(1);
 		}
-        listFib[N] = listFib[N-1].add(listFib[N-2]);
+		if (listFib[N] == null) {
+			listFib[N] = calcF(N - 1).add(calcF(N - 2));
+		}
 		return listFib[N];
-    }
-    
-    public static void main(String[] args)
-    {
-        for (int N = 0; N < 100; N++)
-            StdOut.println(N + " " + F(N));
-    } 
+	}
+
+	public static void main(String[] args) {
+		for (int N = 0; N < 100; N++) {
+			StdOut.println(N + " " + F(N));
+		}
+	}
 }
