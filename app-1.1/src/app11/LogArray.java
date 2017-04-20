b070470214c493b89222b2e98126eaa0e6d79a10
derokarf <derokarf@gmail.com>
2017-04-20 15:08:24 +0800

    init commit

---
+++ app-1.1/src/app11/LogArray.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app11;
+
+/**
+ *
+ * @author igor
+ */
+public class LogArray {
+
+	public static boolean[] createLA(int N){
+		boolean[] la = new boolean[N];
+		
+		
+		return la;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
