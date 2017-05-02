bdc88ad7108d060397b4afcca7425854d094ce15
derokarf <derokarf@gmail.com>
2017-05-02 18:04:04 +0800

    init commit

---
+++ app-1.1/src/chapter12/Rational.java
@@ -0,0 +1,37 @@
+package chapter12;
+
+public class Rational {
+
+    protected long _numerator;
+    protected long _denominator;
+    public Rational(int numerator,int denominator) {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+    
+    public Rational plus(Rational b){
+        
+    }
+    
+    public Rational minus(Rational b){
+        
+    }
+    
+    public Rational times(Rational b){
+        
+    }
+    
+    public Rational divides(Rational b){
+        
+    }
+    
+    public Rational equals(Rational that){
+        
+    }
+    
+    @Override
+    public String toString(){
+        
+    }
+    
+}
