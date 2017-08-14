e2bfddc8b96db94c99b243df4c7f52119f949016
derokarf <derokarf@gmail.com>
2017-08-14 14:32:02 +0800

    complete

--- app-1.1/src/chapter14/SortArrayN.java
+++ app-1.1/src/chapter14/SortArrayN.java
@@ -15,12 +15,20 @@ public class SortArrayN {
 
         int iB = 0;
         int iA = 0;
-        int a, b;
+        int a, b, res;
         while ((iA < list1.length) && (iB < list2.length)) {
             a = list1[iA];
             b = list2[iB];
             if (a == b) {
-                result.add(a);
+                res = a;
+                //игнорирование подряд идущих одинаковых пар
+                if (result.isEmpty()) {
+                    result.add(res);
+                } else {
+                    if (result.get(result.size() - 1) != a) {
+                        result.add(res);
+                    }
+                }
                 iB++;
                 iA++;
             } else if (a > b) {
@@ -33,36 +41,36 @@ public class SortArrayN {
         return result;
     }
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите размер первого массива");
         int[] list1 = new int[scan.nextInt()];
         int i;
         for (i = 0; i < list1.length; i++) {
-            list1[i] = (int)(Math.random() * 20);
+            list1[i] = (int) (Math.random() * 20);
         }
 
         System.out.println("Введите размер второго массива");
         int[] list2 = new int[scan.nextInt()];
         for (i = 0; i < list2.length; i++) {
-            list2[i] = (int)(Math.random() * 20);
-        }        
-        
+            list2[i] = (int) (Math.random() * 20);
+        }
+
         //сортировка
         Arrays.sort(list1);
         Arrays.sort(list2);
-        
+
         StringBuilder strBF = new StringBuilder("Первый массив: ");
-                
-        for(i=0;i<list1.length;i++){
+
+        for (i = 0; i < list1.length; i++) {
             strBF.append(list1[i]);
             strBF.append(" ");
         }
-        
+
         System.out.println(strBF.toString());
         strBF = new StringBuilder("Второй массив: ");
-                
-        for(i=0;i<list2.length;i++){
+
+        for (i = 0; i < list2.length; i++) {
             strBF.append(list2[i]);
             strBF.append(" ");
         }
@@ -71,11 +79,11 @@ public static void main(String[] args) {
         SortArrayN testFindCommon = new SortArrayN();
         ArrayList<Integer> result = testFindCommon.getCommonValues(list1, list2);
         strBF = new StringBuilder("Общие элементы: ");
-                
-        for(i=0;i<result.size();i++){
+
+        for (i = 0; i < result.size(); i++) {
             strBF.append(result.get(i));
             strBF.append(" ");
         }
-        System.out.println(strBF.toString());        
+        System.out.println(strBF.toString());
     }
 }
